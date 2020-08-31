53f0103afc033de3f0498c1e20b1a43d69f13285
syntakks <syntakks@gmail.com>
2020-08-30 22:43:20 -0500

    Cleaned up bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const { token, prefix, nasa_key } = require("./config.json");
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const commands = ['!commands', '!ping', '!server', '!userinfo', '!chuck-norris', '!nasa']; 
+const commands = ['!commands', '!server', '!userinfo', '!chuck-norris', '!nasa']; 
 
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -29,10 +29,6 @@ client.on('message', async message => {
         message.reply('GO EAT TACOS'); 
         return;
     }
-    if (command === `${prefix}ping`) {
-        ping(message); 
-        return;
-    }
     if (command === `${prefix}server`) {
         server(message); 
         return;
@@ -55,173 +51,158 @@ client.on('messageDelete', async message => {
     console.log('Message Deleted', message); 
 });
 
-async function getCommands(msg) {
+async function getCommands(message) {
     try {
-        const exampleEmbed = new Discord.MessageEmbed()
+        const messageEmbed = new Discord.MessageEmbed()
             .attachFiles(['./assets/robot.png'])
-            .setColor('#0099ff')
-            .setTitle('How can I help you?')
-            .setURL('https://discord.js.org/')
-            .setAuthor('synba', 'attachment://robot.svg')
-            .setDescription('Your Server Assistant')
             .setThumbnail('attachment://robot.png')
-            .addField('Commands', commands)
-            .setImage('attachment://robot.svg')
-            .setTimestamp()
-            .setFooter(`${prefix}commands`);
-        msg.reply(exampleEmbed);
+            .setColor('#F7931E')
+            .setAuthor('syntaks.io', 'http://syntaks.io/img/blueLogo.png', 'http://syntaks.io')
+            .setTitle('GET Commands')
+            .setURL('http://syntaks.io')
+            .setDescription('Listing bot commands...')
+            .addField('Commands:', commands)
+            .setFooter(`${prefix}commands`)
+            .setTimestamp();
+        message.reply(messageEmbed);
     }
-    catch(err) {
-        console.log(err); 
+    catch(error) {
+        console.log(error); 
     }
 }
 
-async function ping(msg) {
+async function server(message) {
+    console.log('server');
+    if (message.channel.type === 'dm') return;
     try {
-        const exampleEmbed = new Discord.MessageEmbed()
+        const messageEmbed = new Discord.MessageEmbed()
             .attachFiles(['./assets/robot.png'])
-            .setColor('#0099ff')
-            .setTitle('How can I help you?')
-            .setURL('https://discord.js.org/')
-            .setAuthor('synba', 'attachment://robot.svg')
-            .setDescription('Your Server Assistant')
             .setThumbnail('attachment://robot.png')
-            .addField('Response', 'pong')
-            .setImage('attachment://robot.svg')
-            .setTimestamp()
-            .setFooter(`${prefix}ping`);
-        msg.reply(exampleEmbed);
+            .setColor('#F7931E')
+            .setAuthor('syntaks.io', 'http://syntaks.io/img/blueLogo.png', 'http://syntaks.io')
+            .setTitle('GET Server Info')
+            .setURL('http://syntaks.io')
+            .setDescription('Fetching server data...')
+            .addField('Server Name:', message.guild.name)
+            .addField('Server Owner:', message.guild.owner)
+            .addField('Total Members:', message.guild.memberCount)
+            .setImage(message.guild.owner.user.avatarURL())
+            .setFooter(`${prefix}server`)
+            .setTimestamp();
+        message.reply(messageEmbed);
     }
-    catch(err) {
-        console.log(err); 
+    catch(error) {
+        console.log(error); 
     }
 }
 
-async function server(msg) {
+async function userInfo(message) {
     try {
-        const exampleEmbed = new Discord.MessageEmbed()
+        const messageEmbed = new Discord.MessageEmbed()
             .attachFiles(['./assets/robot.png'])
-            .setColor('#0099ff')
-            .setTitle('How can I help you?')
-            .setURL('https://discord.js.org/')
-            .setAuthor('synba')
-            .setDescription('Your Server Assistant')
             .setThumbnail('attachment://robot.png')
-            .addField('Server Name', msg.guild.name)
-            .addField('Server Owner', msg.guild.owner)
-            .addField('Total Members', msg.guild.memberCount)
-            .setImage(msg.guild.owner.avatarURL)
-            .setTimestamp()
-            .setFooter(`${prefix}server`);
-        msg.reply(exampleEmbed);
+            .setColor('#F7931E')
+            .setAuthor('syntaks.io', 'http://syntaks.io/img/blueLogo.png', 'http://syntaks.io')
+            .setTitle('User Info')
+            .setURL('http://syntaks.io')
+            .setDescription('Fetching user data...')
+            .addField('Full Username:', `${message.author.username}#${message.author.discriminator}`)
+            .addField('User ID:', message.author.id)
+            .addField('Created @:', message.author.createdAt)
+            .setImage(message.author.avatarURL())
+            .setFooter(`${prefix}userinfo`)
+            .setTimestamp();
+        message.reply(messageEmbed);
     }
-    catch(err) {
-        console.log(err); 
+    catch(error) {
+        console.log(error); 
     }
 }
 
-async function userInfo(msg) {
-    try {
-        const exampleEmbed = new Discord.MessageEmbed()
-            .attachFiles(['./assets/robot.png'])
-            .setColor('#0099ff')
-            .setTitle('How can I help you?')
-            .setURL('https://discord.js.org/')
-            .setAuthor('synba')
-            .setDescription('Your Server Assistant')
-            .setThumbnail('attachment://robot.png')
-            .addField('Your Username', msg.author.username)
-            .addField('User ID', msg.author.id)
-            .setImage(msg.author.avatarURL)
-            .setTimestamp()
-            .setFooter(`${prefix}user-info`);
-        msg.reply(exampleEmbed);
-    }
-    catch(err) {
-        console.log(err); 
-    }
-}
-
-async function getChuckJoke(msg) {
-    https.get('https://api.chucknorris.io/jokes/random', (resp) => {
+async function getChuckJoke(message) {
+    https.get('https://api.chucknorris.io/jokes/random', (response) => {
     let data = '';
 
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-        data += chunk;
-    });
+        // A chunk of data has been recieved.
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
 
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-        const json = JSON.parse(data); 
-        console.log('chuck-norris', json.value); 
-        try {
-            const exampleEmbed = new Discord.MessageEmbed()
-                .attachFiles(['./assets/robot.png'])
-                .setColor('#0099ff')
-                .setTitle('Fucking Chuck Norris!')
-                .setURL('https://discord.js.org/')
-                .setAuthor('synba')
-                .setDescription('Your Server Assistant')
-                .setThumbnail('attachment://robot.png')
-                .addField('Chuck Norris Joke', json.value)
-                .setTimestamp()
-                .setFooter(`${prefix}chuck-norris`);
-            msg.reply(exampleEmbed);
-        }
-        catch(err) {
-            console.log(err); 
-        }
-    });
-
-    }).on("error", (err) => {
-    console.log("Error: " + err.message);
-    });
-}
-
-async function getNasaPicOfDay(msg) {
-    https.get(`https://api.nasa.gov/planetary/apod?api_key=${nasa_key}`, (resp) => {
-    let data = '';
-
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-        data += chunk;
-    });
-
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-        try {
+        // The whole response has been received. Print out the result.
+        response.on('end', () => {
             const json = JSON.parse(data); 
-            var explanation = ""; 
-            if (json.explanation.length > 500) {
-                explanation = json.explanation.substring(0, 500) + 
-                '...See full description: https://apod.nasa.gov/apod/astropix.html'; 
-            } else {
-                explanation = json.explanation +
-                '...See full description: https://apod.nasa.gov/apod/astropix.html';
+            console.log('chuck-norris', json.value); 
+            try {
+                const messageEmbed = new Discord.MessageEmbed()
+                    .attachFiles(['./assets/robot.png'])
+                    .setThumbnail('attachment://robot.png')
+                    .setColor('#F7931E')
+                    .setAuthor('syntaks.io', 'http://syntaks.io/img/blueLogo.png', 'http://syntaks.io')
+                    .setTitle('Chuck F&$*ing Norris!')
+                    .setURL('https://www.youtube.com/watch?v=zj2Zf9tlg2Y')
+                    .setDescription('Fetching joke data...')
+                    .addField('Facts:', json.value)
+                    .setFooter(`${prefix}chuck-norris`)
+                    .setTimestamp();
+                message.reply(messageEmbed);
             }
-            console.log('nasa-pic-of-day', json);
-            const exampleEmbed = new Discord.MessageEmbed()
-                .attachFiles(['./assets/robot.png'])
-                .setColor('#0099ff')
-                .setTitle(json.title)
-                .setURL('https://discord.js.org/')
-                .setAuthor('synba')
-                .setDescription('Nasa Pic of the Day')
-                .setThumbnail('attachment://robot.png')
-                .addField('Description', explanation)
-                .setTimestamp()
-                .setImage(json.hdurl)
-                .setFooter(`${prefix}nasa`);
-            msg.reply(exampleEmbed);
-        }
-        catch(err) {
-            console.log(err); 
-        }
+            catch(error) {
+                console.log(error); 
+            }
+        });
+    })
+    .on("error", (error) => {
+    console.log("Error: " + error.message);
     });
+}
 
-    }).on("error", (err) => {
-    console.log("Error: " + err.message);
+async function getNasaPicOfDay(message) {
+    https.get(`https://api.nasa.gov/planetary/apod?api_key=${nasa_key}`, (resonse) => {
+    let data = '';
+
+        // A chunk of data has been recieved.
+        resonse.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resonse.on('end', () => {
+            try {
+                const json = JSON.parse(data); 
+                if (json.code === 404) {
+                    message.reply('No image data available today...');
+                    return;
+                }
+                var explanation = ""; 
+                if (json.explanation.length > 500) {
+                    explanation = json.explanation.substring(0, 500) + 
+                    '...See full description: https://apod.nasa.gov/apod/astropix.html'; 
+                } else {
+                    explanation = json.explanation +
+                    '...See full description: https://apod.nasa.gov/apod/astropix.html';
+                }
+                
+                const exampleEmbed = new Discord.MessageEmbed()
+                    .attachFiles(['./assets/robot.png'])
+                    .setColor('#F7931E')
+                    .setTitle(json.title)
+                    .setURL('https://discord.js.org/')
+                    .setAuthor('synba')
+                    .setDescription('Nasa Pic of the Day')
+                    .setThumbnail('attachment://robot.png')
+                    .addField('Description', explanation)
+                    .setImage(json.hdurl)
+                    .setFooter(`${prefix}nasa`)
+                    .setTimestamp();
+                message.reply(exampleEmbed);
+            }
+            catch(error) {
+                console.log(error); 
+            }
+        });
+    })
+    .on("error", (error) => {
+    console.log("Error: " + error.message);
     });
 }
 
@@ -229,6 +210,6 @@ async function getNasaPicOfDay(msg) {
 try {
     client.login(token);
 }
-catch (err) {
-    console.log(err); 
+catch (error) {
+    console.log(error); 
 }
